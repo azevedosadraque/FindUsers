3d16743ce86545de7cda53665a846d2bd1e75ba3
Sadraque Azevedo <sadraque.azevedo@hotmail.com>
2024-10-06 12:50:35 -0300

    feat: add routes for search and user-repos components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { UserReposComponent } from './user-repos/user-repos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: SearchComponent },
+  { path: 'user/:username', component: UserReposComponent },
+];
